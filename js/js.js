e8d4d9918866bd9b7302af80ad6adf95d00be383
Sarah Choi <sun.eye.rah@gmail.com>
2017-07-26 13:40:16 -0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -8,13 +8,13 @@ $(function(){
 
 	var menu = 
 		'<ul class="menulist"> \
-		<li style="margin-top: 20px; "><a id= "sarahchoi" href="http://www.sarahsunchoi.com/">S A R A H &nbsp; S U N &nbsp; C H O I</a></li> \
-		<li style="margin-top: 10px;"><a href="http://www.sarahsunchoi.com/about.html">a b o u t</a></li> \
+		<li style="margin-top: 20px; "><a id= "sarahchoi" href="http://sarahsunchoi.com/">S A R A H &nbsp; S U N &nbsp; C H O I</a></li> \
+		<li style="margin-top: 10px;"><a href="http://sarahsunchoi.com/about.html">a b o u t</a></li> \
 		<li style="margin-top: 40px;"> \
 		<li style="margin-top: 40px;"> \
-		<li><a href="http://www.sarahsunchoi.com/painting.html">P A I N T I N G</a></li> \
-		<li><a href="http://www.sarahsunchoi.com/graphic.html">G R A P H I C</a></li> \
-		<li><a href="http://www.sarahsunchoi.com/installation.html">I N S T A L L A T I O N</a> \
+		<li><a href="http://sarahsunchoi.com/painting.html">P A I N T I N G</a></li> \
+		<li><a href="http://sarahsunchoi.com/graphic.html">G R A P H I C</a></li> \
+		<li><a href="http://sarahsunchoi.com/installation.html">I N S T A L L A T I O N</a> \
 		<li style="margin-top: 40px;"><a href="https://www.facebook.com/sarahsunchoiart/">f a c e b o o k</a></li> \
 		<li><a href="https://instagram.com/su.un_/">i n s t a g r a m</a></li> \
 		<li><a href="https://vimeo.com/suun">v i m e o</a></li> \
